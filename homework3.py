33dd9a23206df0123e7c7b11ca85af32f3ab428a
Oleg <otrishin@gmail.com>
2024-06-16 19:01:59 +1000

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+name = "Oleg"
+print ("Name:" , name)
+age =42
+print ("Age:", age)
+age = 42 #Перезапись в age текущее значение
+age = 45 #Просто перезапись на неверное
+print ("New age:", age)
+is_student = True
+print ("Is Student:", is_student)
