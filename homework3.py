fac8c10412184a1652830c08ae5f4e7a27a8997c
Oleg <otrishin@gmail.com>
2024-06-18 20:12:39 +1000

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,9 +1,8 @@
 name = "Oleg"
-print ("Name:" , name)
-age =42
-print ("Age:", age)
-age = 42 #Перезапись в age текущее значение
-age = 45 #Просто перезапись на неверное
-print ("New age:", age)
+print("Name:", name)
+age = 42
+print("Age:", age)
+age = age + 1
+print("New age:", age)
 is_student = True
-print ("Is Student:", is_student)
+print("Is Student:", is_student)
